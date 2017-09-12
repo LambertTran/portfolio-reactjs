9e20240bc9b652bd88acc0e2b52db88ecb35d124
lam tran <lb.tran648@gmail.com>
2017-09-12 12:26:09 -0700

    add project images

--- src/components/projectpage/projectpage.js
+++ src/components/projectpage/projectpage.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 import NavBar from '../share/navbar';
 import ProjectItem from './project-item';
 
+/** images **/
 import project1 from './images/screen.png';
+import wikisearch from './images/wiki-search.png';
+import markdown from './images/markdown.png';
+
+
 class ProjectPage extends Component{
   render(){
 
@@ -11,8 +16,8 @@ class ProjectPage extends Component{
         <NavBar />
         <div className="container">
           <div className="row">
-            <ProjectItem img={project1} path="/projects/wikisearch" name='wiki' />
-            <ProjectItem img={project1} path="/projects/mardownpreview" name='markdown' />
+            <ProjectItem img={wikisearch} path="/projects/wikisearch" name='wiki' />
+            <ProjectItem img={markdown} path="/projects/mardownpreview" name='markdown' />
             <ProjectItem img={project1} path="/" />
             <ProjectItem img={project1} path="/" />
             <ProjectItem img={project1} path="/" />
