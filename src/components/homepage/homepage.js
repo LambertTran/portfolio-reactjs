175b6b7eb9f5a2e6e62ee041c78ec924f82f2b7b
lam tran <lb.tran648@gmail.com>
2017-09-15 19:19:48 -0700

    add particles effect in homepage

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,13 +1,22 @@
 import React,{Component} from 'react';
-
+import 'particles.js/particles';
 /** import components **/
 import NavBar from '../share/navbar';
 import HomeContents from './homepage-contents';
+const particlesJS = window.particlesJS;
 
 class HomePage extends Component{
+
+  componentDidMount(){
+    particlesJS.load('particles-js', 'particle.json', function() {
+      console.log('callback - particles-js config loaded');
+    });
+  }
+
   render(){
+    
     return(
-      <div>
+      <div id="particles-js">
         <NavBar />
         <HomeContents />
       </div>
